45fab5bb242429c1d95e16b6f9819e11ad575eda
Justin Cooper <justinwcooper@outlook.com>
2024-06-07 17:56:19 -0400

    updates text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl bg-gradient-to-l from-orange-500 to-yellow-300 text-transparent bg-clip-text text-center">Matthew 13:37</h1>
-      <p className="text-m text-gray-700 px-4">He answered, “The one who sows the good seed is the Son of Man.”</p>
+      <h1 className="text-6xl bg-gradient-to-l from-orange-500 to-yellow-300 text-transparent bg-clip-text text-center">Matthew 13:37</h1>
+      <p className="text-xs px-4"><sup>37</sup>He answered, “The one who sows the good seed is the Son of Man.”</p>
     </main>
   );
 }
