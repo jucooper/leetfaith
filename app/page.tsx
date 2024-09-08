6b2bb960d4de1c5c67cc5b64162637714f7aab64
Justin Cooper <justinwcooper@outlook.com>
2024-09-08 14:54:55 -0400

    center text for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center animate-fadeIn">
       <h1 className="text-6xl bg-gradient-to-l from-orange-500 to-yellow-300 text-transparent bg-clip-text text-center">Matthew 13:37</h1>
-      <p className="text-xs px-4"><sup>37</sup>He answered, “The one who sows the good seed is the Son of Man.”</p>
+      <p className="text-xs px-4 text-center md:text-left"><sup>37</sup>He answered, “The one who sows the good seed is the Son of Man.”</p>
     </main>
   );
 }
