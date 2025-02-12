2e8d4d8421706dce36fcac9337c755ab946f26de
Justin Cooper <justinwcooper@outlook.com>
2025-02-12 08:10:29 -0500

    removes logo and story links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,17 +16,6 @@ const Navbar = () => {
         <nav className="fixed top-3 left-0 w-full z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
-            <div className="flex-shrink-0">
-                <Link href="/">
-                <Image
-                    src="/leetfaith.png"
-                    alt="LeetFaith Logo"
-                    width={40}
-                    height={40}
-                    className="object-contain"
-                />
-                </Link>
-            </div>
 
             <div className="block lg:hidden">
                 <button
@@ -48,15 +37,6 @@ const Navbar = () => {
                     />
                 </svg>
                 </button>
-            </div>
-
-            <div className="hidden lg:flex lg:items-center lg:space-x-6">
-                <Link
-                href="/about"
-                className="hover:text-gray-500 px-4 py-2"
-                >
-                Story
-                </Link>
             </div>
             </div>
         </div>
